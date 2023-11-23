02a6c07b35f102d7af27bb16441b007a16d6b8f6
XMintGreenX <36018@ma-web.nl>
2023-11-23 20:37:09 +0100

    2.4 huisje

---
+++ 02 huisje/app.js
@@ -0,0 +1,59 @@
+class App
+{ 
+    runApplication()
+    {
+        console.log("hello world!");
+        let canvas = document.getElementById("canvasId");
+        console.log(canvas);
+        let g = canvas.getContext("2d");
+        g.beginPath();
+        g.fillStyle = "red";
+        g.strokeStyle = "black";
+        g.moveTo(30,10);
+        g.lineTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath();
+        g.fillStyle = "orange";
+        g.strokeStyle = "black";
+        g.moveTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(80,30);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath();
+        g.fillStyle = "grey";
+        g.strokeStyle = "black";
+        g.moveTo(20,30);
+        g.lineTo(20,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
+        g.moveTo(60,40);
+        g.lineTo(80,30);
+        g.lineTo(80,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath();
+        g.fillStyle = "white";
+        g.strokeStyle = "black";
+        g.beginPath();
+        g.moveTo(30,36);
+        g.lineTo(30,42);
+        g.lineTo(38,44);
+        g.lineTo(38,38);
+        g.closePath();
+        g.stroke();
+    }
+}
+
+let app = new App();
+app.runApplication();
+
