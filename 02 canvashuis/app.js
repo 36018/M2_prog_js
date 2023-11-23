7afe608653ff4abe995436fc884cfe45163cc953
XMintGreenX <36018@ma-web.nl>
2023-11-23 14:49:06 +0100

    2.2 kleurtje

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -6,6 +6,9 @@ class App
         let canvas = document.getElementById("canvasId");
         console.log(canvas);
         let g = canvas.getContext("2d");
+        g.fillStyle = "#CFF0CC";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "pink";
         g.fillRect(0,0,10,10);
     }
 }
