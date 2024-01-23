aee0f0b7a19c57198ca147669ed34c600f3d4cfa
XMintGreenX <36018@ma-web.nl>
2024-01-23 14:12:30 +0100

    5.2

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -4,22 +4,39 @@ class App
     {
         console.log("hello world!");
         const array = ["één", "twee", "drie"];
-        console.log (array);
+        console.log (array[0] + " " + array[1] + " " + array[2]);
+
+        array.push("bob dylan");
+        array.push("prince");
 
         for (let i = 0; i < array.length; i++){
             const element = array[i];
-            console.log(i + ": "+ array);
+            console.log(i + ": "+ element);
         }
+
+        let indexToRemove = array.indexOf("bob dylan");
+        array.splice(indexToRemove,1);
+
+        console.log (array);
+
+        let indexToRemove2 = array.indexOf("prince");
+        array.splice(indexToRemove2,2);
+
+        console.log (array);
+
+        array.push("vier");
+        array.push("vijf");
+
+        console.log (array);
+
 
         const nummer = [2, 5, 7];
         console.log (nummer);
 
-        for (let i = 0; i < 2; i++){
+        for (let i = 0; i < array.length; i++){
             const numbers = nummer[i];
             console.log (numbers + 1);
         }
-
-       
 
     }
 }
