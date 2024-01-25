95af015dd916bca456f76124c2454715ed22a6e3
XMintGreenX <36018@ma-web.nl>
2024-01-25 15:34:43 +0100

    6 af

--- 06 events/app.js
+++ 06 events/app.js
@@ -7,8 +7,48 @@ class App
         
         uiButton.addEventListener("click",(e)=>
         {
-            console.log("click!");
+            const para = document.createElement("p");
+            const node = document.createTextNode("This is new.");
+            para.appendChild(node);
+            document.body.appendChild(para);
         });
+
+        let auButton = (document.getElementById("ThisButton"))
+
+        auButton.addEventListener("click",(e)=>
+        {
+            const header = document.createElement("h1");
+            const headertext = document.createTextNode("Wie is dit?");
+            header.appendChild(headertext);
+            document.body.appendChild(header);
+            const para = document.createElement("p");
+            const node = document.createTextNode("Dit is....");
+            para.appendChild(node);
+            document.body.appendChild(para);
+            let link = document.createElement("a");
+            link.href = "https://www.kidzsupplies.nl/roommates-roommates-muursticker-mickey-mouse-xl.html";
+            link.alt = "Mickey die zwaait.";
+            const para3 = document.createElement("img");
+            para3.src = "image.jpg";
+            link.appendChild(para3);
+            document.body.appendChild(link);
+        });
+            let button = document.getElementById("button");
+            button.style.backgroundColor = "pink";
+            let pButton = document.getElementById("button");
+            pButton.addEventListener("click", (e)=>
+        {
+            const para1 = document.createElement("h1");
+            const node1 = document.createTextNode("clicked a div");
+            para1.appendChild(node1);
+            document.body.appendChild(para1);
+            const para2 = document.createElement("p");
+            const node2 = document.createTextNode("wow!");
+            para2.appendChild(node2);
+            document.body.appendChild(para2);
+        });
+            
+        
     
     }
 }
