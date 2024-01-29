5ebd8aa7cde1f4f984dfd7575dd96c767e71a198
XMintGreenX <36018@ma-web.nl>
2024-01-29 10:08:11 +0100

    7.1

---
+++ 07 classes/app.js
@@ -0,0 +1,34 @@
+class Greet
+{
+    constructor()
+    {
+        console.log("Greetings!");
+    }
+}
+
+let greet = new Greet();
+new Greet();
+new Greet();
+new Greet();
+
+class GoodBye
+{
+    constructor()
+    {
+        console.log("Bye Bye");
+    }
+}
+
+let bye = new GoodBye();
+
+class App
+{ 
+    runApplication()
+    {
+        console.log("hello world!");
+        
+    }
+}
+
+let app = new App();
+app.runApplication();
